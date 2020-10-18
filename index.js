ed9b664fb4b329db4a21e8c0e448c786072b0ac0
Renda02 <rendaniluvhengo89@gmail.com>
2020-10-18 09:48:37 +0200

    added 10 more products

--- index.js
+++ index.js
@@ -31,23 +31,50 @@ const tomato = new Product("Tomato", 1.5, "Vegetable", 3);
 const cheese = new Product("Cheese", 3.5, "Dairy", 2);
 const banana = new Product("Banana", 1, "Fruit", 2);
 const orange = new Product("Orange", 5, "Fruit", 50);
-const avocado = new Product("Avogado", 3, "Fruit", 15);
+const avocado = new Product("Avocado", 3, "Fruit", 15);
+const blueberry = new Product("Blueberry", 2, "Fruit", 10);
+const lettuce = new Product("Lettuce", 1, "Vegetable", 4);
+const olives = new Product("Olives", 3, "Vegetable", 20);
+const mango = new Product("Mango", 2, "Fruit", 6);
+const mushroom = new Product("Mushrrom", 4, "Vegetable", 13);
+const tofu = new Product("Tofu", 4, "Dairy", 2);
+const churro = new Product("Churros", 1, "Dessert", 7);
+const carrot = new Product("Carrot", 1, "Vegetable", 6);
+const beans = new Product("Chea-peas", 2, "Vegetable", 2);
+const milk = new Product("Soy", 5, "Dairy", 6);
+const seed = new Product("Chai-seeds", 6, "Seed", 5);
 
-const listProducts = [orange, avocado, muffin, tomato, cheese];
+const listProducts = [
+  orange,
+  avocado,
+  muffin,
+  tomato,
+  cheese,
+  blueberry,
+  olives,
+  lettuce,
+  tofu,
+  mushroom,
+  mango,
+  beans,
+  carrot,
+  milk,
+  seed,
+];
 
 listProducts
-    .sort((a,b)=>{
-        return  b.price - a.price
-    })
-    .forEach((element) => {
-  element.display();
-});
+  .sort((a, b) => {
+    return b.price - a.price;
+  })
+  .forEach((element) => {
+    element.display();
+  });
 
 // create some html to disply it
 // add the html to our page
 
 // TODO:
-// add 10 more products
+// add 10 more products -DONE.
 // add your own style to the shop page
 // add a header and a footer
 // add if the product is bio or not
@@ -56,4 +83,4 @@ listProducts
 // Create a bio-fruit class based on the product one: This will always eb bio and fruit at the same time
 
 // Advanbced
-// filter: add a button that filter the fruits 
+// filter: add a button that filter the fruits
